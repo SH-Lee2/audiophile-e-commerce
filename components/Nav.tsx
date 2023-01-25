3323f2953438e858868071619e40bf70d3d43889
이시형 <tlgud585@gmail.com>
2023-01-26 05:24:46 +0900

    Style: props로 style 전달받게 변경 footer 도 같이 사용하기 때문에 style 전달받게 변경

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,60 +3,60 @@
 import Link from "next/link";
 import { useSelectedLayoutSegment } from "next/navigation";
 
-const Nav = () => {
+const Nav = ({ styles }: { styles?: string }) => {
 	const segment = useSelectedLayoutSegment();
 
 	return (
-		<nav className="hidden xl:inline-block">
-			<ul className="flex  space-x-8 text-[13px] font-[700] leading-[25px] tracking-[2px] ">
-				<li>
-					<Link
-						href="/"
-						className={
-							segment === null ? "text-peru" : "text-white hover:text-tangelo"
-						}
-					>
-						HOME
-					</Link>
-				</li>
-				<li>
-					<Link
-						href="/headphones"
-						className={
-							segment === "headphones"
-								? "text-peru"
-								: "text-white hover:text-tangelo"
-						}
-					>
-						HEADPHONES
-					</Link>
-				</li>
-				<li>
-					<Link
-						href="/speakers"
-						className={
-							segment === "speakers"
-								? "text-peru"
-								: "text-white hover:text-tangelo"
-						}
-					>
-						SPEAKERS
-					</Link>
-				</li>
-				<li>
-					<Link
-						href="/earphones"
-						className={
-							segment === "earphones"
-								? "text-peru"
-								: "text-white hover:text-tangelo"
-						}
-					>
-						EARPHONES
-					</Link>
-				</li>
-			</ul>
-		</nav>
+		<ul
+			className={`${styles} flex text-[13px] font-[700] leading-[25px] tracking-[2px] md:space-x-8`}
+		>
+			<li>
+				<Link
+					href="/"
+					className={
+						segment === null ? "text-peru" : "text-white hover:text-tangelo"
+					}
+				>
+					HOME
+				</Link>
+			</li>
+			<li>
+				<Link
+					href="/headphones"
+					className={
+						segment === "headphones"
+							? "text-peru"
+							: "text-white hover:text-tangelo"
+					}
+				>
+					HEADPHONES
+				</Link>
+			</li>
+			<li>
+				<Link
+					href="/speakers"
+					className={
+						segment === "speakers"
+							? "text-peru"
+							: "text-white hover:text-tangelo"
+					}
+				>
+					SPEAKERS
+				</Link>
+			</li>
+			<li>
+				<Link
+					href="/earphones"
+					className={
+						segment === "earphones"
+							? "text-peru"
+							: "text-white hover:text-tangelo"
+					}
+				>
+					EARPHONES
+				</Link>
+			</li>
+		</ul>
 	);
 };
 
