c6bb4a8349db57e53421a2f81112881a0b1f3b43
이시형 <tlgud585@gmail.com>
2023-02-03 05:06:04 +0900

    Feat:  데이터 타입 정의 받아오는 스키마의 데이터 타입 정의

---
+++ typings.d.ts
@@ -0,0 +1,50 @@
+type Base = {
+	_createdAt: string;
+	_id: string;
+	_rev: string;
+	_type: string;
+	_updateAt: string;
+};
+
+interface Banner extends Base {
+	buttonText: string;
+	desc: string;
+	product: Product;
+	image: Array<Image>;
+	smallText: string;
+	link: string;
+}
+
+interface Product extends Base {
+	category: Reference;
+	des: string;
+	features: Array<string>;
+	image: Array<Image>;
+	inTheBox: Array<InTheBox>;
+	name: string;
+	newProduct: boolean;
+	price: number;
+	productDesImage: Array<Image>;
+	slug: Slug;
+	smallText: string;
+}
+
+interface Image {
+	_type: "image";
+	asset: Reference;
+}
+
+interface Reference {
+	_ref: string;
+	_type: "reference";
+}
+
+interface InTheBox {
+	amount: string;
+	components: string;
+}
+
+interface Slug {
+	_type: "slug";
+	current: string;
+}
